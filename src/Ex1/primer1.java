0af5a2dfa73f83268c769dbeef9b060341674aef
Hakuto0 <lstanislav439@gmail.com>
2026-01-11 20:01:46 +0500

    Initial commit

--- src/Ex1/primer1.java
+++ src/Ex1/primer1.java
@@ -1,7 +1,29 @@
 package Ex1;
 
+import java.io.File;
+
 public class primer1 {
-    static void main(String[] args) {
-        System.out.println("Привет мир!!");;
+    public static void main(String[] args) {
+        try {
+            // Создание файла в текущей папке
+            File f1 = new File("MyFile1.txt");
+            f1.createNewFile();
+            if (f1.exists()) {
+                System.out.println("Создан!!!!");
+                System.out.println("Полный путь1: " + f1.getAbsolutePath());
+            }
+
+            // Создание файла на диске C
+            File f2 = new File("D:\\MyFile2.txt");
+            f2.createNewFile();
+            System.out.println("Полный путь 2: " + f2.getAbsolutePath());
+
+            // Создание нескольких вложенных папок
+            File f3 = new File("D:\\Papka1\\Papka2\\Papka3");
+            f3.mkdirs();
+            System.out.println("Полный путь 3: " + f3.getAbsolutePath());
+        } catch (Exception e) {
+            System.out.println("Ошибка!!! " + e);
+        }
     }
 }
